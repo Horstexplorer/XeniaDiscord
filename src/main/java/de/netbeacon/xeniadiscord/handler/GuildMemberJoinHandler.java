59432b469692a8acae88a3d6dd747d1d8ab738ad
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2019-05-10 01:04:52 +0200

    added some more welcome messages

--- src/main/java/de/netbeacon/xeniadiscord/handler/GuildMemberJoinHandler.java
+++ src/main/java/de/netbeacon/xeniadiscord/handler/GuildMemberJoinHandler.java
@@ -19,10 +19,15 @@ public class GuildMemberJoinHandler implements Runnable{
             {
                 // prepare messages
                 String[] messages = {
-                        "Hey, "+event.getUser().getName()+" and welcome on "+ event.getGuild().getName()+"."+"\n"+"Have a nice day and a lot of fun on the server!"+"\n"+"With kind regards, Xenia."+"\n",
-                        "Hello "+event.getUser().getName()+" nice to meet you."+"\n"+"Glad you made it to "+ event.getGuild().getName() +"\n"+"Have a nice time here!"+"\n"+"Sincerely, Xenia."+"\n",
-                        "Hi "+event.getUser().getName()+"!"+"\n"+"Nice to see you here on "+event.getGuild().getName()+"."+"\n"+"Best regards, Xenia."+"\n",
-                        "Welcome to "+event.getGuild().getName()+", "+event.getUser().getName()+"!"+"\n"+"With kind regards, Xenia."+"\n"
+                        "Hey, "+event.getUser().getName()+" and welcome on "+ event.getGuild().getName()+"."+"\n"+"Have a nice day and a lot of fun on the server!"+"\n"+"With kind regards, Xenia"+"\n",
+                        "Hello "+event.getUser().getName()+" nice to meet you."+"\n"+"Glad you made it to "+ event.getGuild().getName() +"\n"+"Have a nice time here!"+"\n"+"Sincerely, Xenia"+"\n",
+                        "Hi "+event.getUser().getName()+"!"+"\n"+"Nice to see you here on "+event.getGuild().getName()+"."+"\n"+"Best regards, Xenia"+"\n",
+                        "Welcome to "+event.getGuild().getName()+", "+event.getUser().getName()+"!"+"\n"+"With kind regards, Xenia"+"\n",
+                        "Welcome to "+event.getGuild().getName()+", enjoy your stay :)"+"\n"+"Xenia"+"\n",
+                        "Hey "+event.getUser().getName()+"!"+"\n"+"We're super excited to have you here!"+"\n"+"Xenia"+"\n",
+                        "Welcome "+event.getUser().getName()+"!"+"\n"+"Thank you for joining "+event.getGuild().getName()+"\n",
+                        "Greetings "+event.getUser().getName()+"!"+"\n"+"Welcome to "+event.getGuild().getName()+"!"+"\n",
+                        "Good to see you here "+event.getUser().getName()+"!"+"\n"
                 };
                 // send message
                 channel.sendMessage(messages[new Random().nextInt(messages.length)]).queue();
