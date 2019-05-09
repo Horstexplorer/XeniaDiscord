c8a09000ac251c2d341bb4234745ead1e9ad0d09
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2019-05-09 13:59:08 +0200

    admin commands now start with admin

--- src/main/java/de/netbeacon/xeniadiscord/commands/privat/Admin.java
+++ src/main/java/de/netbeacon/xeniadiscord/commands/privat/Admin.java
@@ -9,47 +9,48 @@ public class Admin implements PrivateCommand {
     @Override
     public void execute(PrivateMessageReceivedEvent event, String[] args) {
         if(event.getAuthor().getId().equals(new Config().load("bot_admin_id"))){
-            // shutdown bot
-            if(args[0].toLowerCase().equals("shutdown")){
-                event.getJDA().shutdownNow();
-                System.exit(0);
-            }
-            // setstatus
-            if(args[0].toLowerCase().equals("onlinestatus")){
-                if (args.length>1){
-                    switch (args[1].toLowerCase()){
-                        case "dnd":
-                            event.getJDA().getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
-                            break;
+            if(args[0].toLowerCase().equals("admin")){
+                // shutdown bot
+                if(args[1].toLowerCase().equals("shutdown")){
+                    event.getJDA().shutdownNow();
+                    System.exit(0);
+                }
+                // setstatus
+                if(args[1].toLowerCase().equals("onlinestatus")){
+                    if (args.length>2){
+                        switch (args[2].toLowerCase()){
+                            case "dnd":
+                                event.getJDA().getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
+                                break;
 
-                        case "idle":
-                            event.getJDA().getPresence().setStatus(OnlineStatus.IDLE);
-                            break;
+                            case "idle":
+                                event.getJDA().getPresence().setStatus(OnlineStatus.IDLE);
+                                break;
 
-                        default:
-                            event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
-                            break;
+                            default:
+                                event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
+                                break;
+                        }
                     }
                 }
-            }
-            // force blacklist save
-            if(args[0].toLowerCase().equals("blacklistforcesave")){
-                if (args.length>1){
-                    if(!new BlackListUtility().writetofile()){
-                        event.getChannel().sendMessage("Saving blacklist failed.").queue();
-                    }else{
-                        event.getChannel().sendMessage("Blacklist saved sucessfully").queue();
+                // force blacklist save
+                if(args[1].toLowerCase().equals("blacklistforcesave")){
+                    if (args.length>2){
+                        if(!new BlackListUtility().writetofile()){
+                            event.getChannel().sendMessage("Saving blacklist failed.").queue();
+                        }else{
+                            event.getChannel().sendMessage("Blacklist saved sucessfully").queue();
+                        }
                     }
                 }
-            }
-            // update config property values
-            // force blacklist save
-            if(args[0].toLowerCase().equals("config")){
-                if (args.length>2){
-                    if(new Config().updateproperties(args[1].toLowerCase(), args[2])){
-                        event.getChannel().sendMessage("Value updated successfull.").queue();
-                    }else{
-                        event.getChannel().sendMessage("Update failed. Please check property name").queue();
+                // update config property values
+                if(args[1].toLowerCase().equals("config")){
+                    if (args.length>3){
+                        if(new Config().updateproperties(args[1].toLowerCase(), args[2])){
+                            event.getChannel().sendMessage("Value updated successfull").queue();
+                        }else{
+                            event.getChannel().sendMessage("Update failed. Please check property name").queue();
+                        }
                     }
                 }
             }
