4e8bbbdf64f8d0669b6247ab8a38471d6ec195a8
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2019-06-27 21:57:47 +0200

    added twitch hooks to BTM

--- src/main/java/de/netbeacon/xeniadiscord/core/BTM.java
+++ src/main/java/de/netbeacon/xeniadiscord/core/BTM.java
@@ -2,6 +2,7 @@ package de.netbeacon.xeniadiscord.core;
 
 import de.netbeacon.xeniadiscord.util.BlackListUtility;
 import de.netbeacon.xeniadiscord.util.Config;
+import de.netbeacon.xeniadiscord.util.webhooks.twitch.TwitchHookHandler;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
@@ -38,9 +39,18 @@ public class BTM implements Runnable{
                 }
             }
         };
-        // shedule tasks
+        TimerTask update_twitchhooks = new TimerTask() {
+            @Override
+            public void run() {
+                if(!new TwitchHookHandler().update()){
+                    System.err.println("Updating twitch-hooks failed.");
+                }
+            }
+        };
+        // schedule tasks
         time.schedule(update_status,1000*60,1000*60);            // wait 1 minute then update every minute
         time.schedule(save_blacklist, 1000*60*60, 1000*60*60);    // wait 1h then update every h
+        time.schedule(update_twitchhooks,1000*60*5,1000*60*5);            // wait 5 minutes then update every 5 minutes
     }
 
     private int user_count(){
--- src/main/java/de/netbeacon/xeniadiscord/util/webhooks/twitch/TwitchHookHandler.java
+++ src/main/java/de/netbeacon/xeniadiscord/util/webhooks/twitch/TwitchHookHandler.java
@@ -1,4 +1,20 @@
 package de.netbeacon.xeniadiscord.util.webhooks.twitch;
 
 public class TwitchHookHandler {
+
+    public TwitchHookHandler(){
+
+    }
+
+    public boolean add(){
+        return false;
+    }
+
+    public boolean remove(){
+        return false;
+    }
+
+    public boolean update(){
+        return true;
+    }
 }
