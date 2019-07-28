7710975333d684869599114826dd12533f536231
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2019-07-28 22:31:51 +0200

    minor fix

--- src/main/java/de/netbeacon/xeniadiscord/core/BTM.java
+++ src/main/java/de/netbeacon/xeniadiscord/core/BTM.java
@@ -48,7 +48,7 @@ public class BTM implements Runnable{
         // schedule tasks
         time.schedule(update_status,1000*60,1000*60);            // wait 1 minute then update every minute
         time.schedule(save_blacklist, 1000*60*60, 1000*60*60);    // wait 1h then update every h
-        time.schedule(update_twitchhooks,1000*30,1000*30);            // wait 5 minutes then update every 5 minutes
+        time.schedule(update_twitchhooks,1000*30*5,1000*30*5);            // wait 5 minutes then update every 5 minutes
     }
 
     private int user_count(){
