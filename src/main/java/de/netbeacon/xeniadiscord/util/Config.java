23877659416f38aebe083e14f6558d89ac822f95
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2019-09-03 19:25:59 +0200

    updated build

--- src/main/java/de/netbeacon/xeniadiscord/util/Config.java
+++ src/main/java/de/netbeacon/xeniadiscord/util/Config.java
@@ -128,7 +128,7 @@ public class Config {
 
     public String version() {
         String vers= "1.0.6.2";
-        String build = "1908220328";
+        String build = "1909031925";
         return vers+"-"+build;
     }
 }
